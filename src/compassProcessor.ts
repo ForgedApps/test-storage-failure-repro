620ff0f8de3eedcc81abb06b4e5dbe7f3130151a
James Gentes <jgentes@gmail.com>
2024-11-14 19:15:20 -0800

    closure fix

--- src/compassProcessor.ts
+++ src/compassProcessor.ts
@@ -3,10 +3,15 @@ import { storage } from '@forge/api'
 export const queueProcessor = async ({ key }: { key: number }) => {
   console.log(`Getting test${key}`)
 
-  const value = await storage.get(`test${key}`)
-  console.log(`Retrieved value for test${key}:`, value)
+  const getStorage = async (key: number) => {
+    const value = await storage.get(`test${key}`)
+    console.log(`Retrieved value for test${key}:`, value)
+    
+    if (key === 3) return console.log('finished, works fine using forge tunnel')
+    
+    await getStorage(key + 1)
+  }
 
-  if (key === 3) return console.log('finished, works fine using forge tunnel')
-
-  queueProcessor({ key: key + 1 })
+  await getStorage(key)
+  console.log('done')
 }
